d9434be9795da4065f04f6176dd19eef458c006b
theNomadxXX <100027921+theNomadxXX@users.noreply.github.com>
2022-12-27 05:13:10 +0800

    Update cart-limit-by-product-condition.php

--- cart-limit-by-product-condition.php
+++ cart-limit-by-product-condition.php
@@ -1,38 +1,40 @@
 <?php
 /*
- * Plugin Name: Cart Limit By Product Condition for Woocommerce
- * Plugin URI:  https://wordpress.org/plugins/cart-limit-by-product-condition-for-woocommerce/
- * Description: Set limits for product consolidation.
- * Version:     1.0.0
- * Author:      LibbyWu
- * Author URI:  https://profiles.wordpress.org/nomadbd/
+ * Plugin Name: Cart Limit By Product Condition
+ * Plugin URI:  
+ * Description: 設定商品合併限制
+ * Text Domain: wcl-plugin
+ * Version:     2.2.1
+ * Author:      nomad ju
+ * Author URI:  
+ * License:     GPL2
+ * License URI: 
  */
 
 defined( 'ABSPATH' ) || exit;
 
 if(! class_exists('cart_limit_by_produt_condition')) {
-
     class cart_limit_by_produt_condition {
-        
         public function __construct() {
-            add_filter('woocommerce_product_data_tabs', array($this, 'create_cart_limit_tab'));
-            add_action('woocommerce_product_data_panels', array($this, 'display_cart_limit_fields'));
-            add_action('woocommerce_process_product_meta', array($this, 'save_fields'));
-            add_action('woocommerce_product_after_variable_attributes', array($this, 'variation_settings_fields'), 10, 3 );
             
+            // Create the custom tab
+            add_filter('woocommerce_product_data_tabs', array($this, 'create_cart_limit_tab'));
+            // Add the custom fields
+            add_action('woocommerce_product_data_panels', array($this, 'display_cart_limit_fields'));
+            // Save tje custom fields
+            add_action('woocommerce_process_product_meta', array($this, 'save_fields'));
+            // Add Variation Settings
+            add_action( 'woocommerce_product_after_variable_attributes', array($this, 'variation_settings_fields'), 10, 3 );
+            //-----------------------------------------
 		    add_action( 'woocommerce_check_cart_items', array($this,'check_cart_limit'),15,0 );
+            // Save Variation Settings
 		    add_action( 'woocommerce_save_product_variation', array($this,'save_variation_settings_fields'),10, 2 );
         }
 
-        /**
-         * Add new tab for clbpc.
-         *
-         * @version 1.0.0
-         * @since   1.0.0
-         */
+
         public function create_cart_limit_tab($tabs) {
             $tabs['clbpc'] = array(
-                'label'     => __('Product Consolidation', 'clbpc'), // tab name
+                'label'     => __('合并订购', 'clbpc'), // tab name
                 'target'    => 'clbpc_panel',// anchor link,
                 'class'     => array('show_if_simple', 'show_if_variable'),
                 'priority'  => 80,
@@ -40,51 +42,43 @@ if(! class_exists('cart_limit_by_produt_condition')) {
             return $tabs;
         }
 
-        /**
-         * display fields.
-         *
-         * @version 1.0.0
-         * @since   1.0.0
-         * @todo hide/show options
-         */
         public function display_cart_limit_fields() { 
+            $product = wc_get_product();
             ?>
                 <div id = 'clbpc_panel' class = 'panel woocommerce_options_panel'>
                     <div class="options_group">
                         <?php
                         woocommerce_wp_checkbox(
                             array(
-                                'id'        => 'include_clbpc_option',
-                                'label'     => __('enable product limit', 'clbpc'),
+                                'id'        => 'clbpc_include_varitaion_limit',
+                                'label'     => __('本賣場內变量群组限制', 'clbpc'),
                                 'desc_tip'    => 'true',
-                                'description'  => __('Limit between other products.', 'clbpc')
+                                'description'  => __('启动变量产品间的订单合并限制，请至变量区块设置群组ID', 'clbpc')
                             )
                         );
                         woocommerce_wp_checkbox(
                             array(
+                                'id'        => 'include_clbpc_option',
+                                'label'     => __('开启與其他賣場的限制', 'clbpc'),
+                            )
+                        );
+                        ?>
+                        <hr>
+                        <?php
+                        woocommerce_wp_checkbox(
+                            array(
                                 'id'        => 'clbpc_include_solo_option',
-                                'label'     => __('is add-on product', 'clbpc'),
+                                'label'     => __('为附加产品', 'clbpc'),
                                 'desc_tip'    => 'true',
-                                'description'  => __('Cannot place an order separately, must combine "non" add-on products to place an order.', 'clbpc')
+                                'description'  => __('不可单独下单，必须合并"非"附加产品下单', 'clbpc')
                             )
                         );
                         woocommerce_wp_text_input(
                             array(
                                 'id'        => 'clbpc_condition_cats',
-                                'label'     => __('allowed categories', 'clbpc'),
+                                'label'     => __('合并的必要分类', 'clbpc'),
                                 'desc_tip'    => 'true',
-                                'description'  => __('Use "," to separate categories, blanks cannot be combined with any product', 'clbpc')
-                            )
-                        );
-                        ?>
-                        <hr/>
-                        <?php
-                        woocommerce_wp_checkbox(
-                            array(
-                                'id'        => 'clbpc_include_varitaion_limit',
-                                'label'     => __('enable variation limit', 'clbpc'),
-                                'desc_tip'    => 'true',
-                                'description'  => __('To activate order merging restrictions between variable products, please go to the variable block to set the group ID', 'clbpc')
+                                'description'  => __('使用英文半角逗号隔开分类，空白则是不可与任何分类合并', 'clbpc')
                             )
                         );
                         ?>
@@ -93,12 +87,6 @@ if(! class_exists('cart_limit_by_produt_condition')) {
             <?php 
         }
 
-        /**
-         * save fields data.
-         *
-         * @version 1.0.0
-         * @since   1.0.0
-         */
         public function save_fields($post_id) {
             $product = wc_get_product($post_id);
 
@@ -118,51 +106,44 @@ if(! class_exists('cart_limit_by_produt_condition')) {
         }
 
         // ------------------------------------------------------------------------------------------
-        /**
-         * set variable group id.
-         *
-         * @version 1.0.0
-         * @since   1.0.0
-         * @todo show/hide with "clbpc_include_varitaion_limit"
-         */
+        //variation
         public function variation_settings_fields( $loop, $variation_data, $variation ) {
+            woocommerce_wp_checkbox(
+                array(
+                'id'        => 'clbpc_variation_addon['. $loop .']',
+                'label'     => __('为附加产品', 'clbpc'),
+                'desc_tip'    => 'true',
+                'description'  => __('不可单独下单，必须合并"非"附加产品下单', 'clbpc'),
+                'value'       => get_post_meta($variation->ID, 'clbpc_variation_addon', true),
+                )
+            );
             // Number Field
             woocommerce_wp_text_input( 
                 array( 
                 'id'          => 'clbpc_variation_group_id['. $loop .']', 
-                'label'       => __( 'variable order limit id', 'clbpc' ), 
+                'label'       => __( '变量群组编号', 'clbpc' ), 
                 'desc_tip'    => true,
-                'wrapper_class' => 'form-row form-row-last',
-                'description' => __( 'Orders with the same group number can be combined in one order', 'clbpc' ),
+                'wrapper_class' => 'form-row',
+                'description' => __( '相同群组编号者可合并订单', 'clbpc' ),
                 'type'        => 'number', 
                 'value'       => get_post_meta($variation->ID, 'clbpc_variation_group_id', true),
                 'custom_attributes' => array(
                         'step'  => '1',
                         'min' => '0'
-                        ) 
+                    ) 
                 )
             );
-            
         }
 
-        /**
-         * save variable group id data.
-         *
-         * @version 1.0.0
-         * @since   1.0.0
-         */
         public function save_variation_settings_fields($variation_id, $i) {
+            $check_field = $_POST['clbpc_variation_addon'][$i];
+            update_post_meta( $variation_id, 'clbpc_variation_addon', esc_attr( $check_field ) );
+
             $number_field = $_POST['clbpc_variation_group_id'][$i];
             update_post_meta( $variation_id, 'clbpc_variation_group_id', esc_attr( $number_field ) );
         }
 
         // ------------------------------------------------------------------------------------------
-        /**
-         * check limit and show error notice.
-         *
-         * @version 1.0.0
-         * @since   1.0.0
-         */
         public function check_cart_limit() {
 		    $is_violation = 0;
             $cart_items = WC()->cart->get_cart();
@@ -185,10 +166,11 @@ if(! class_exists('cart_limit_by_produt_condition')) {
                 }
             }
 		    if ($is_violation) {
-		        wc_add_notice(__('Does not meet product combination specifications, please check and delete non-mergeable products', 'clbpc'), 'error');
+		        wc_add_notice(__('不符合商品合并规范，请检查并删除不可合并/不可单独的商品', 'clbpc'), 'error');
             }
         }
 
+        // 與其他產品
         private function is_other_item_fix_limit($condition_cats, $cart_items, $cart_item, $solo_limit) {
             $is_pass = 0;
             foreach( $cart_items as $other_cart_item ){
@@ -202,13 +184,22 @@ if(! class_exists('cart_limit_by_produt_condition')) {
             return 1;
         }
 
+        // 單一產品
         private function is_same_item_fix_limit($cart_items, $cart_item) {
             $product = wc_get_product($cart_item['product_id']);
             if($product->get_type() == "variable" && !empty($product->get_available_variations())) {
+                $is_add_on = get_post_meta($cart_item['variation_id'], 'clbpc_variation_addon', true);
+                $is_pass = 0;
                 foreach( $cart_items as $other_cart_item ){
                     if($other_cart_item['product_id'] == $cart_item['product_id']) {
-                        if (!self::is_fix_varitation_id($cart_item, $other_cart_item)) return 0;
+                        if (!self::is_fix_varitation_id($cart_item, $other_cart_item)) return 0; // 變數群組
+                        if (!get_post_meta($other_cart_item['variation_id'], 'clbpc_variation_addon', true)) {
+                            $is_pass += 1;
+                        }
                     }
+                }
+                if ($is_add_on and $is_pass < 1) {
+                    return 0;
                 }
             }
             return 1;
